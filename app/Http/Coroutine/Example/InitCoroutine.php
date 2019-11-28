75914409fa5eceaeb9e03d58073923a33a9f3cff
linyiyuan <linyiyuann@163.com>
2019-11-28 18:10:27 +0800

    '增加协程配置'

--- app/Http/Coroutine/Example/InitCoroutine.php
+++ app/Http/Coroutine/Example/InitCoroutine.php
@@ -19,9 +19,7 @@ class InitCoroutine extends BaseCoroutine
         //Swoole\Runtime::enableCoroutine()作用是将PHP提供的stream、sleep、pdo、mysqli、redis等功能从同步阻塞切换为协程的异步IO
         //当使用co::sleep co::pdo 这种可以实现同样的效果，同样是切换成协程异步IO
         \Swoole\Runtime::enableCoroutine();
-        \Swoole\Coroutine::set([
-            'max_coroutine' => 1,
-        ]);
+
 
         $coroutineId1 = go(function () {
             \Co::sleep(1);
@@ -34,10 +32,25 @@ class InitCoroutine extends BaseCoroutine
         });
 
         echo $coroutineId1;
-
     }
 
-
+    /**
+     * 设置coroutine 配置项
+     *
+     */
+    protected static function setOption()
+    {
+        \Swoole\Coroutine::set([
+            'max_coroutine' => 1, //设置最大协程数，超过限制后底层将无法创建新的协程。
+            'stack_size' => '2049', //设置单个协程初始栈的内存尺寸，默认为2M
+            'log_level' => SWOOLE_LOG_TRACE, //日志等级
+            'trace_flags' => SWOOLE_TRACE_ALL, //跟踪标签
+            'socket_connect_timeout' => 1, //建立socket连接超时时间, 默认为1(秒)
+            'socket_timeout' => -1, //socket读写操作超时时间, 默认为-1, 即永不超时
+            'dns_cache_expire' => 60, //设置swoole dns缓存失效时间,单位秒,默认60秒
+            'dns_cache_capacity' => 1000, //设置swoole dns缓存容量,默认1000
+        ]);
+    }
 
 
 
